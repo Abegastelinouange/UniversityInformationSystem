ff0e4fcaca44151b097dbbae4963b3b20de1d28f
Abegastelinouange <abegaprincesse204@gmail.com>
2024-12-05 14:16:07 +0100

    Add files via upload

---
+++ University.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+
+public class University {
+	
+	 private final String name;
+
+	    public University(String name) {
+	        this.name = name;
+	    }
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+	    public String getName() {
+	        return this.name;
+	    }
+
+    void addDepartment(Department department) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
